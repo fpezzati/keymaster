1bbb71819f495c1304c2ff8e884cfa9f34b42b5e
fpezzati <fpezzati@gmail.com>
2024-08-11 17:43:28 +0200

    Finally I got a positive interation.

--- src/oauth2.rs
+++ src/oauth2.rs
@@ -1,18 +1,18 @@
-use axum::response::IntoResponse;
-use reqwest::header::ACCEPT;
-use serde_json::Value;
-use serde_json::json;
-use serde::{Deserialize, Serialize};
-use axum::response::Response;
-use axum::http::StatusCode;
 use axum::http::header;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::response::Response;
 use axum::Json;
 use cookie::Cookie;
+use jwt_simple::algorithms::RSAKeyPairLike;
 use jwt_simple::claims::Claims;
 use jwt_simple::prelude::Duration;
 use jwt_simple::prelude::RS384KeyPair;
-use jwt_simple::algorithms::RSAKeyPairLike;
-use log::{ info, debug, error };
+use log::{debug, error};
+use reqwest::header::ACCEPT;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use serde_json::Value;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct OAuth2Conf {
@@ -28,7 +28,7 @@ impl OAuth2Conf {
             client_id: conf["client_id"].as_str().unwrap().to_string(),
             redirect_uri: conf["redirect_uri"].as_str().unwrap().to_string(),
             client_secret: conf["client_secret"].as_str().unwrap().to_string(),
-            idp_url: conf["idp_url"].as_str().unwrap().to_string()
+            idp_url: conf["idp_url"].as_str().unwrap().to_string(),
         }
     }
 }
@@ -42,7 +42,12 @@ pub async fn request_token(oauth2_conf: Value, code: String, private_key: String
     debug!("Ready to fetch token: {token_url}");
     let client = reqwest::Client::new();
     debug!("preparing token request");
-    match client.get(token_url.as_str()).header(ACCEPT, "application/json").send().await {
+    match client
+        .get(token_url.as_str())
+        .header(ACCEPT, "application/json")
+        .send()
+        .await
+    {
         Ok(resp) => {
             let resp_status = resp.status();
             debug!("response: {resp_status}");
@@ -51,47 +56,80 @@ pub async fn request_token(oauth2_conf: Value, code: String, private_key: String
                     Ok(json_body) => {
                         debug!("request went fine.");
                         return pick_token_and_provides_response(json_body, private_key);
-                    },
+                    }
                     Err(e) => {
                         // no token, raise error
                         error!("No token, error returned.");
-                        return build_error_response(StatusCode::INTERNAL_SERVER_ERROR, format!("No token found. Original error: {}", e).to_string());
+                        return build_error_response(
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("No token found. Original error: {}", e).to_string(),
+                        );
                     }
                 }
             } else if resp.status() == 301 {
                 debug!("Redirect detected.");
-                let redirect_uri : String = resp.headers().get("Location").unwrap().to_str().unwrap().to_string();
-                match client.post(redirect_uri.as_str()).header("Accept", "application/json").send().await {
+                let redirect_uri: String = resp
+                    .headers()
+                    .get("Location")
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string();
+                match client
+                    .post(redirect_uri.as_str())
+                    .header("Accept", "application/json")
+                    .send()
+                    .await
+                {
                     Ok(redirect_resp) => {
                         debug!("Redirect went fine.");
                         match redirect_resp.json::<Value>().await {
                             Ok(redirect_body) => {
                                 debug!("Getting token from redirect.");
-                                return pick_token_and_provides_response(redirect_body, private_key);
-                            },
+                                return pick_token_and_provides_response(
+                                    redirect_body,
+                                    private_key,
+                                );
+                            }
                             Err(redirect_err) => {
                                 // something is wrong with redirect reply, raise error
                                 error!("Error while getting redirect response body.");
-                                return build_error_response(StatusCode::INTERNAL_SERVER_ERROR, format!("No token found. Original error: {}", redirect_err).to_string());
+                                return build_error_response(
+                                    StatusCode::INTERNAL_SERVER_ERROR,
+                                    format!("No token found. Original error: {}", redirect_err)
+                                        .to_string(),
+                                );
                             }
                         }
-                    },
+                    }
                     Err(e) => {
                         // redirect went wrong, raise error
                         error!("Redirect went wrong.");
-                        return build_error_response(StatusCode::INTERNAL_SERVER_ERROR, format!("No token found. Original error: {}", e).to_string());
+                        return build_error_response(
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("No token found. Original error: {}", e).to_string(),
+                        );
                     }
                 }
             } else {
                 // get token went wrong, raise error
                 let resp_body = resp.text().await.unwrap();
                 error!("Getting token went wrong, Idp provider replies with an error: {resp_body}");
-                return build_error_response(StatusCode::INTERNAL_SERVER_ERROR, format!("Error while fetching token. Original response: {}", resp_body));
+                return build_error_response(
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!(
+                        "Error while fetching token. Original response: {}",
+                        resp_body
+                    ),
+                );
             }
-        },
+        }
         Err(err) => {
             error!("Token request went wrong.");
-            return build_error_response(StatusCode::INTERNAL_SERVER_ERROR, format!("Error while fetching token. Original error: {}", err).to_string());
+            return build_error_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Error while fetching token. Original error: {}", err).to_string(),
+            );
         }
     }
 }
@@ -100,44 +138,54 @@ pub async fn request_token(oauth2_conf: Value, code: String, private_key: String
 struct UserClaims {
     user: String,
     auth_provider: String,
-    token: String
+    token: String,
 }
 
 fn pick_token_and_provides_response(body_as_json: Value, private_key: String) -> Response {
     debug!("Body from which to pick access token: {body_as_json}");
     let token = body_as_json["access_token"].as_str().unwrap();
-    let cookie = Cookie::build(("hey", token)).secure(true).http_only(true).build();
+    let cookie = Cookie::build(("hey", token))
+        .secure(true)
+        .http_only(true)
+        .build();
     let cookie_value = String::from(cookie.value());
+    debug!("Private key: {private_key}");
     let pkey = RS384KeyPair::from_pem(private_key.as_str()).unwrap();
 
-    let unsigned_claims = Claims::with_custom_claims(UserClaims {
-      user: String::from("jimmy"),
-      auth_provider: String::from("github"),
-      token: token.to_string()
-    }, Duration::from_hours(1));
+    let unsigned_claims = Claims::with_custom_claims(
+        UserClaims {
+            user: String::from("jimmy"),
+            auth_provider: String::from("github"),
+            token: token.to_string(),
+        },
+        Duration::from_hours(1),
+    );
     let signed_claims = pkey.sign(unsigned_claims).unwrap();
 
     return (
-      StatusCode::OK,
-      [(header::SET_COOKIE, signed_claims), (header::CONTENT_TYPE, "application/json".to_string())],
-      Json(json!({
-        "msg": cookie_value
-      }))
-    ).into_response();
+        StatusCode::OK,
+        [
+            (header::SET_COOKIE, signed_claims),
+            (header::CONTENT_TYPE, "application/json".to_string()),
+        ],
+        Json(json!({
+          "msg": cookie_value
+        })),
+    )
+        .into_response();
 }
 
-fn build_error_response(status_code : StatusCode, error_msg : String) -> Response {
+fn build_error_response(status_code: StatusCode, error_msg: String) -> Response {
     #[derive(Serialize, Deserialize)]
     struct ErrorResponsePayload {
-      error: String
+        error: String,
     }
-  
-    let error_msg_as_json = ErrorResponsePayload {
-      error: error_msg
-    };
+
+    let error_msg_as_json = ErrorResponsePayload { error: error_msg };
     return (
-      status_code,
-      [(header::CONTENT_TYPE, "application/json".to_string())],
-      Json(serde_json::to_value(error_msg_as_json).unwrap())
-    ).into_response();
+        status_code,
+        [(header::CONTENT_TYPE, "application/json".to_string())],
+        Json(serde_json::to_value(error_msg_as_json).unwrap()),
+    )
+        .into_response();
 }
--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,15 @@
-use log::info;
-use serde_json::Value;
 use axum::{
-    extract::{Path, State, Query}, 
-    http::StatusCode, 
-    response::IntoResponse, 
-    routing::get, 
-    Json, 
-    Router
+    extract::{Path, Query, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Json, Router,
 };
-use tokio::net::TcpListener;
+use log::{debug, info};
+use serde_json::Value;
+use std::fs;
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
 use crate::oauth2;
 
@@ -19,34 +19,44 @@ pub struct ServerConfig {
     port: String,
     public_key: String,
     private_key: String,
-    pub oauth2_conf: String
+    pub oauth2_conf: String,
 }
 
 impl ServerConfig {
     pub async fn start(config: &str) {
         println!("{}", config);
-        let json_content : Value = serde_json::from_str(&config).expect("Invalid file.");
+        let json_content: Value = serde_json::from_str(&config).expect("Invalid file.");
+        let public_key_content = json_content["public_key"].to_string();
+        let private_key_content =
+            fs::read_to_string(String::from(json_content["private_key"].as_str().unwrap()))
+                .unwrap();
         let server = ServerConfig {
             host: json_content["host"].as_str().unwrap().to_string(),
             port: json_content["port"].to_string(),
-            public_key: json_content["public_key"].to_string(),
-            private_key: json_content["private_key"].to_string(),
-            oauth2_conf: json_content["oauth2_conf"].to_string()
+            public_key: public_key_content,
+            private_key: private_key_content,
+            oauth2_conf: json_content["oauth2_conf"].to_string(),
         };
-        log4rs::init_file(json_content["log_conf"].as_str().unwrap(), Default::default()).unwrap();
+        log4rs::init_file(
+            json_content["log_conf"].as_str().unwrap(),
+            Default::default(),
+        )
+        .unwrap();
 
         let mut hostport = String::new();
         hostport.push_str(server.host.as_str());
         hostport.push_str(":");
         hostport.push_str(server.port.as_str());
         println!("hostport: {}", hostport);
-        let server_socket = hostport.parse::<SocketAddr>().expect("invalid host:port pair");
+        let server_socket = hostport
+            .parse::<SocketAddr>()
+            .expect("invalid host:port pair");
 
         let routes = Router::new()
-          .route("/", get(hello))
-//          .route("/verify", post(verify))
-          .route("/callback/:id_provider", get(callback))
-          .with_state(server);
+            .route("/", get(hello))
+            //          .route("/verify", post(verify))
+            .route("/callback/:id_provider", get(callback))
+            .with_state(server);
         let listener = TcpListener::bind(server_socket).await.unwrap();
 
         axum::serve(listener, routes).await.unwrap();
@@ -59,16 +69,22 @@ async fn hello(State(server): State<ServerConfig>) -> impl IntoResponse {
 
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct CallbackAuthParams {
-  code: String
+    code: String,
 }
 
 pub async fn callback(
     State(server_config): State<ServerConfig>,
     Path(id_provider): Path<String>,
-    Query(params): Query<CallbackAuthParams>
+    Query(params): Query<CallbackAuthParams>,
 ) -> impl IntoResponse {
     info!("id_provider: {}", id_provider.as_str());
-    let oauth2_config: Value = serde_json::from_str(server_config.oauth2_conf.as_str()).expect("Invalid configuration.");
+    let oauth2_config: Value =
+        serde_json::from_str(server_config.oauth2_conf.as_str()).expect("Invalid configuration.");
     let oauth2_config_provider = oauth2_config[id_provider.as_str()].clone();
-    oauth2::request_token(oauth2_config_provider, params.code, server_config.private_key).await
+    oauth2::request_token(
+        oauth2_config_provider,
+        params.code,
+        server_config.private_key,
+    )
+    .await
 }
